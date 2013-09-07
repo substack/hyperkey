1126eda02eedfcbf181938de87b7c293ef75ca65
James Halliday <mail@substack.net>
2013-09-07 15:43:06 +0100

    moved the assoc example into render-assoc

--- example/assoc/render/hacker.js
+++
@@ -1,9 +0,0 @@
-var hyperkey = require('../../../');
-var fs = require('fs');
-var html = fs.readFileSync(__dirname + '/hacker.html');
-
-module.exports = function () {
-    return hyperkey(html, function (row) {
-        return { '.name': row.name };
-    });
-};
