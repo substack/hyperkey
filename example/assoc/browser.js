beb3da707c3a16abebe5f9b5c935ec6369106c63
James Halliday <mail@substack.net>
2013-09-07 15:42:17 +0100

    moved magic() into render-assoc

--- example/assoc/browser.js
+++ example/assoc/browser.js
@@ -1,34 +1,10 @@
-var parse = require('level-assoc/parse');
-var through = require('through');
-
 var shoe = require('shoe');
 var sock = shoe('/sock');
+var rassoc = require('render-assoc');
 
 var render = require('./render/hackerspace.js')();
 render.appendTo('#hackerspaces');
 
-render.pipe(sock).pipe(magic(render, {
-    hacker: { key: 'hackerspace', render: require('./render/hacker.js') }
+render.pipe(sock).pipe(rassoc(render, {
+    hacker: require('./render/hacker.js')
 }));
-
-function magic (render, types) {
-    var streams = {};
-    render.on('element', function (elem) {
-        var key = elem.getAttribute('data-key');
-        streams[key] = {};
-        Object.keys(types).forEach(function (k) {
-            streams[key][k] = types[k].render().appendTo(elem);
-        });
-    });
-    
-    var p = parse();
-    p.pipe(through(function (row) {
-        var t = row && row.value && row.value.type;
-        var s = t && types[t] && streams[row.value[types[t].key]];
-        if (s && s[t]) {
-            s[t].write(row);
-        }
-        else render.write(row);
-    }));
-    return p;
-}
